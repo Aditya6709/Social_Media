bb5d18c2e4a6fcb45817f5a13a5c103e9875e1ec
jaychauhan-exe <jaychauhan.exe@gmail.com>
2025-01-26 13:02:31 +0530

    dashboard + header changes

---
+++ pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/header'
+export default function kutta() {
+  return (
+    <div>
+        <div className='main'>
+            <Header />
+        </div>
+
+    </div>
+  )
+}
